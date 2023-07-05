b981b31255d6febd698d383ba36c586fb06d67c4
Jokorie <jokorie@mit.edu>
2023-07-05 19:27:29 +0000

    blur working

--- src/blur.ml
+++ src/blur.ml
@@ -1,8 +1,30 @@
 open Core
 
-(* You need to modify this function to blur the input image
-   based on the provided radius instead of ignoring it. *)
-let transform image ~radius:_ = image
+(* You need to modify this function to blur the input image based on the
+   provided radius instead of ignoring it. *)
+let transform image ~radius =
+  let blurred_image = Image.copy image in
+  let _ =
+    Array.init (Image.width image) ~f:(fun x ->
+      Array.init (Image.height image) ~f:(fun y ->
+        let x_start = if x - radius < 0 then 0 else x - radius in
+        let x_end =
+          if x + radius > Image.width image - 1
+          then Image.width image
+          else x + radius
+        in
+        let y_start = if y - radius < 0 then 0 else y - radius in
+        let y_end =
+          if y + radius > Image.height image - 1
+          then Image.height image
+          else y + radius
+        in
+        let slice = Image.slice image ~x_start ~x_end ~y_start ~y_end in
+        let avg = Image.mean_pixel slice in
+        Image.set blurred_image ~x ~y avg))
+  in
+  blurred_image
+;;
 
 let command =
   Command.basic
@@ -24,5 +46,6 @@ let command =
         let image' = transform image ~radius in
         Image.save_ppm
           image'
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_blur.ppm")]
 ;;
