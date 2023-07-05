c76047dc06655de3ab2585ac9f55937925689956
Jokorie <jokorie@mit.edu>
2023-07-05 21:16:24 +0000

    dither incomplete

--- src/dither.ml
+++ src/dither.ml
@@ -1,7 +1,23 @@
 open Core
 
 (* This should look familiar by now! *)
-let transform image = image
+
+let in_bounds ~image ~x ~y =
+  if x > 0 && x< Image.width image -1 
+    && y > 0 && y < Image.height image - 1 
+  then true
+  else false
+;;
+
+let transform image =
+  let gray_image = Grayscale.transform image in 
+  Image.mapi gray_image ~f:( fun ~x ~y (r,g,b) -> 
+    let difference = r - Image.max_val gray_image /2 in
+     if in_bounds ~image:gray_image ~x:x+1 ~y then 
+      () 
+    else ()
+    ) 
+;;
 
 let command =
   Command.basic
@@ -17,5 +33,6 @@ let command =
         let image = Image.load_ppm ~filename |> transform in
         Image.save_ppm
           image
-          ~filename:(String.chop_suffix_exn filename ~suffix:".ppm" ^ "_dither.ppm")]
+          ~filename:
+            (String.chop_suffix_exn filename ~suffix:".ppm" ^ "_dither.ppm")]
 ;;
